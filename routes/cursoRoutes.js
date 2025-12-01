16a9f792a280e0812ce1b42389874bc041ff9f9d
lea <leandroromero68@gmail.com>
2025-12-01 05:18:44 -0300

    Actualice cursoRoutes porque no andaba el servidor

--- routes/cursoRoutes.js
+++ routes/cursoRoutes.js
@@ -1,26 +1,14 @@
 import express from "express";
-import usuarioController from "../controllers/usuarioController.js";
+import cursoController from "../controller/cursoController.js";
 
 const router = express.Router();
-const userController = new usuarioController();
+const controller = new cursoController();
 
-// ================================
-// Rutas de usuarios
-// ================================
-
-// Obtener todos los usuarios
-router.get("/", (req, res) => userController.getUsuarios(req, res));
-
-// Crear usuario (registrar)
-router.post("/", (req, res) => userController.addUsuario(req, res));
-
-// Obtener usuario por ID
-router.get("/:id", (req, res) => userController.getUsuarioById(req, res));
-
-// Actualizar usuario por ID
-router.put("/:id", (req, res) => userController.updateUsuarioById(req, res));
-
-// Eliminar usuario por ID
-router.delete("/:id", (req, res) => userController.deleteUsuarioById(req, res));
+// Endpoints para cursos
+router.get("/", controller.getCursos);               // Obtener todos los cursos
+router.get("/:id", controller.getCursoById);        // Obtener curso por ID
+router.post("/", controller.addCurso);              // Crear un nuevo curso
+router.put("/:id", controller.updateCursoById);     // Actualizar curso por ID
+router.delete("/:id", controller.deleteCursoById);  // Eliminar curso por ID
 
 export default router;
